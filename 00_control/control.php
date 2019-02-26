b2716d9361bf26ee169205e2b8ab7bccf2c3dc39
Yakup Ates <Yakup.Ates@rub.de>
2019-02-26 18:16:15 +0100

    setter

--- 00_control/control.php
+++ 00_control/control.php
@@ -17,6 +17,36 @@ include __DIR__ . '/../01_model/messages.php';
     public function getMaxCount() {
         return $this->mCount;
     }
+
+    public function setMaxDepth($depth) {
+        if ($depth !== NULL) {
+            $this->mDepth = $depth;
+        }
+    }
+
+    public function setMaxCount($count) {
+        if ($count !== NULL) {
+            $this->mCount = $count;
+        }
+    }
+
+    /**
+     * Function to set dangerLevel
+     * NOTE: dangerLevel is not used for now.
+     */
+    public function setDangerLevel($dangerlevel) {
+        if (is_int($dangerlevel)) {
+            $this->dangerLevel = $dangerlevel;
+        }
+    }
+
+    /**
+     * Function to set callbackurls
+     */
+    public function setCallbackurls($callbackurls) {
+        $this->callbackurls = $callbackurls;
+    }
+
     /**
      * Function to access dangerLevel
      * NOTE: dangerLevel is not used for now.
@@ -46,6 +76,36 @@ include __DIR__ . '/../01_model/messages.php';
         return $this->userAgent;
     }
     /**
+     * Set the user agent individually
+     * Default: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36
+     */
+    public function setUserAgent($agent) {
+        if (!empty($agent)) {
+            $this->userAgent = $agent;
+        } else {
+            /**
+             * Default user agent
+             */
+            $agent  = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) ";
+            $agent .= "AppleWebKit/537.36 (KHTML, like Gecko) ";
+            $agent .= "Chrome/60.0.3112.113 Safari/537.36";
+
+            $this->userAgent = $agent;
+        }
+    }
+    /**
+     * Function to set the error message.
+     */
+    public function setErrorMessage($id, $values) {
+        if (is_int($id)) {
+            $placeholder = $this->messages->getNameById($id);
+
+            $this->errorMessage = array("placeholder" => (string)$placeholder[0],
+                                        "values" => $values);
+        }
+    }
+
+    /**
      * Function to check if the error message.
      */
     public function getErrorMessage() {
@@ -53,6 +113,14 @@ include __DIR__ . '/../01_model/messages.php';
     }
 
 
+    /**
+     * Function to indicate that there was an error.
+     */
+    public function setHasError($hasError=FALSE) {
+        if (is_bool($hasError)) {
+            $this->hasError = $hasError;
+        }
+    }
 
     /**
      * Function to check if the scanner had an error.
