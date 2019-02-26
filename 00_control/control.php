4c698f9320257060e0a227e61c630baff156492c
Yakup Ates <Yakup.Ates@rub.de>
2019-02-26 18:14:28 +0100

    constructor

--- 00_control/control.php
+++ 00_control/control.php
@@ -1,1 +1,12 @@
 include __DIR__ . '/../01_model/messages.php';
+    public function __construct($url, $ua, $limit_defaults) {
+        $this->messages = new Messages();
+        $this->setUserAgent($ua);
+
+        $this->mDepth = $limit_defaults["mDepth"];
+        $this->mCount = $limit_defaults["mCount"];
+
+        $this->url = $url;
+        $this->punycode_url = $this->punycodeUrl($url);
+        $this->punycode_url = $this->checkURL($this->punycode_url);
+    }
